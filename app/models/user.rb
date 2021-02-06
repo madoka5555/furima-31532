8dd8c974887ee124f961242b13926eaa06ca4236
madoka5555 <73727829+madoka5555@users.noreply.github.com>
2021-02-06 12:51:32 +0900

    新規登録/ログイン

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+  validates :first_name
+  validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+  validates :first_name_katakana
+  validates :last_name_katakana
+  end
+    validates :birthday, presence: true
+end

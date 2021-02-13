29fa8f26bd05f3e9be6d51bca896f0afa954ba28
madoka5555 <73727829+madoka5555@users.noreply.github.com>
2021-02-13 17:58:02 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,21 @@
 class ItemsController < ApplicationController
-  def index
-    
+  before_action :authenticate_user!, except: [:index]
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :status_id, :price, :delivery_fee_id, :prefecture, :delivery_day_id, :image).merge(user_id: current_user.id)
   end
 end

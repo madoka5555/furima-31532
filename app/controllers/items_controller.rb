e776ee14e8b5be3ce639b8b78a7f23664043e916
madoka5555 <73727829+madoka5555@users.noreply.github.com>
2021-02-21 19:37:08 +0900

    商品出品機能　修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
+  def index
+    @items = Item.all.order('created_at DESC')
+  end
+
   def new
     @item = Item.new
   end
-
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -13,9 +16,7 @@ class ItemsController < ApplicationController
       render :new
     end
   end
-
   private
-
   def item_params
     params.require(:item).permit(:name, :detail, :category_id, :status_id, :price, :delivery_fee_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
   end

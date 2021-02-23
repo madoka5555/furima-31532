0ed8b055def08085f45c00d25379292672df8c6a
madoka5555 <73727829+madoka5555@users.noreply.github.com>
2021-02-23 17:41:45 +0900

    商品出品機能　修正11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_katakana, :last_name_katakana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :first_name, :last_name, :first_name_katakana, :last_name_katakana,
+                                             :birthday])
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -115,4 +115,3 @@ RSpec.describe Item, type: :model do
     end
   end
 end
-

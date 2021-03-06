807a8744adce261dd2cd54f0d6da12c51591c1b3
madoka5555 <73727829+madoka5555@users.noreply.github.com>
2021-03-06 18:53:53 +0900

    商品削除機能　修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :find_item_id, only: [:show, :edit, :update, :destroy]
-  before_action :item_user_id, only: [:edit, :update]
+  before_action :item_user_id, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')

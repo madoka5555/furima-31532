d4ca576152ae04d6cdd8f3311de2db5f2a225ba4
madoka5555 <73727829+madoka5555@users.noreply.github.com>
2021-03-13 19:28:11 +0900

    商品購入機能　事前準備

---
+++ db/migrate/20210313095030_create_purchases.rb
@@ -0,0 +1,9 @@
+class CreatePurchases < ActiveRecord::Migration[6.0]
+  def change
+    create_table :purchases do |t|
+      t.integer :price  ,null: false
+      
+      t.timestamps
+    end
+  end
+end
